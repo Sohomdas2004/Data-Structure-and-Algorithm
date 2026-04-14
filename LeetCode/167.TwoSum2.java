012725c711b8901ab14262bb79a55daa73308980
Sohomdas2004 <sohomdas1510@gmail.com>
2026-04-14 08:15:06 +0530

    initial commit

---
+++ LeetCode/167.TwoSum2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int res[]=new int[2];
+        int start,end;
+        start=0;
+        end=numbers.length-1;
+        res[0]=-1;
+        res[1]=-1;
+
+        while(start<=end){
+            if(numbers[start]+numbers[end]==target){
+                res[0]=start+1;
+                res[1]=end+1;
+                break;
+            }
+            else if(numbers[start]+numbers[end]<target){
+                start+=1;
+            }
+            else{
+                end-=1;
+            }
+        }
+
+        return res;
+    }
+}
